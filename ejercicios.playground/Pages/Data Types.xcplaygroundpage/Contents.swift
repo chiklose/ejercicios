ab13101844c935d4269faa5abebaa51473240644
Robert Flores <chiklose@MacBook-Air-de-Robert.local>
2021-10-06 17:27:14 -0500

    add: Add a new bool value change true for false

--- ejercicios.playground/Pages/Data Types.xcplaygroundpage/Contents.swift
+++ ejercicios.playground/Pages/Data Types.xcplaygroundpage/Contents.swift
@@ -2,14 +2,14 @@
 
 import Foundation
 
-
+//Rob
 //Data Types
 
 var welcomeMessage: String = "Welcome"
-var isEnabled: Bool = true
+var isEnabled: Bool = false
 let pi: Double = 3.1416
 let area: Int = 13
 var fistVar: String
 var secondVar: Bool
 var thirdVar: String
-secondVar = false
+secondVar = true
