db972b80c80dd870bc946b5ecf17ea93a8a67263
chiklose <81770184+chiklose@users.noreply.github.com>
2021-10-06 17:10:42 -0500

    Add files via upload

---
+++ ejercicios.playground/Pages/Data Types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: [Previous](@previous)
+
+import Foundation
+
+
+//Data Types
+
+var welcomeMessage: String = "Welcome"
+var isEnabled: Bool = true
+let pi: Double = 3.1416
+let area: Int = 13
+var fistVar: String
+var secondVar: Bool
+var thirdVar: String
+secondVar = false
